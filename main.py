d4ecf2c735aba894d54a28875b514b810b05d854
RafekGorgay <rafekrafat21@gmail.com>
2020-12-22 16:29:30 -0500

    copying files

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
+from os import listdir
+from os.original import isfile, join
 import shutil
+
+
+# print the current dir
 print(os.getcwd())
 
+# setting the Original and destination
 original = r'C:\Users\gorraf02\Desktop\Billing'
-target = r'C:\Users\Ron\Desktop\Test_2\new_image.jpg'
+target = r'C:\Users\gorraf02\Desktop'
+
+# Print how many files are there
 print (len([name for name in os.listdir(os.chdir(original))]))
 
+# adding the files names in a list
+onlyfiles = [f for f in listdir(original) if isfile(join(original, f))]
+print (onlyfiles)
 
-#for i in range()
-#shutil.copy()
+# copying the files to the destination
+for i in onlyfiles:
+    shutil.copy(i,target)
