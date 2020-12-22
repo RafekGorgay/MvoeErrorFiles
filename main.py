9c87788bc68c063e4cc7df3525c06d4aa12a749c
RafekGorgay <rafekrafat21@gmail.com>
2020-12-22 16:14:02 -0500

    count number of files

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
-from os import path
+import shutil
+print(os.getcwd())
 
-print(os.name)
+original = r'C:\Users\gorraf02\Desktop\Billing'
+target = r'C:\Users\Ron\Desktop\Test_2\new_image.jpg'
+print (len([name for name in os.listdir(os.chdir(original))]))
+
+
+#for i in range()
+#shutil.copy()
