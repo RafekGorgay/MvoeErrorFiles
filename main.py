1217d8149de870fc26b4bdb7431da824a0da2ca0
RafekGorgay <rafekrafat21@gmail.com>
2020-12-22 12:59:46 -0500

    test6

--- main.py
+++ main.py
@@ -2,20 +2,4 @@ import os
 from os import path
 
 def main ()
-    try:
-        pass
-    except expression as identifier:
-        pass
-    else:
-        pass
-
-    try:
-        pass
-    except expression as identifier:
-        pass
-    else:
-        pass
-    finally:
-        pass
-
-    TimeoutError
+    
