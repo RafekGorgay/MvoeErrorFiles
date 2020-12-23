748e83b5dc7cb5d7e2443efd0fdad15126a8df54
RafekGorgay <rafekrafat21@gmail.com>
2020-12-23 08:12:17 -0500

    test

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import os
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 import shutil
 
 
-# copying the files to the destination
+# Function takes the list of files and the destination and copies the list of files int o the destination
 def copyFiles (list, destination):
     for i in list:
         shutil.copy(i,destination)
 
-        
+
+# setting the Original and destination
+original = os.getcwd()
+target = r'C:\Users\gorraf02\Desktop\Billing\test'
+
+
+
 # print the current dir
 print(os.getcwd())
 
-# setting the Original and destination
-original = r'C:\Users\gorraf02\Desktop\Billing'
-target = r'C:\Users\gorraf02\Desktop\Billing\test'
+foldersName = [name for name in listdir(original) if isdir(join(original, name))]
 
-# Print how many files are there
-print (len([name for name in os.listdir(os.chdir(original))]))
+# calculate how many folders are there
+numberOfFiles = len(foldersName)
+print (numberOfFiles)
+
 
 # adding the files names in a list
 onlyfiles = [f for f in listdir(original) if isfile(join(original, f))]
 print (onlyfiles)
 
-copyFiles(onlyfiles, target)
+## copyFiles(onlyfiles, target)
 
 
