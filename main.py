6c793ed2395d13a67c8a7a766da5c9aba1921c10
RafekGorgay <rafekrafat21@gmail.com>
2020-12-23 12:49:54 -0500

    test

--- main.py
+++ main.py
@@ -3,33 +3,25 @@ from os import listdir
 from os.path import isfile, join, isdir
 import shutil
 
-
 # Function takes the list of files and the destination and copies the list of files int o the destination
 def copyFiles (list, destination):
     for i in list:
         shutil.copy(i,destination)
 
+# setting the Original and destination from the user
 
-# setting the Original and destination
-original = os.getcwd()
-target = r'C:\Users\gorraf02\Desktop\Billing\test'
+original = input("Please enter the location of error files : ")
+#target = input("Please enter the location of current billing month : ")
 
-
-
-# print the current dir
-print(os.getcwd())
-
+# calculate how many folders are there and savinf them in a list
 foldersName = [name for name in listdir(original) if isdir(join(original, name))]
-
-# calculate how many folders are there
-numberOfFiles = len(foldersName)
-print (numberOfFiles)
+numberOfFolders = len(foldersName)
 
 
 # adding the files names in a list
 onlyfiles = [f for f in listdir(original) if isfile(join(original, f))]
-print (onlyfiles)
+numberOfFiles = len(onlyfiles)
+
+
 
 ## copyFiles(onlyfiles, target)
-
-
